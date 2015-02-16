2e2bebe41c067cb357087323e077a046ed4f4857
mattheweallen <mattheweallen@gmail.com>
2015-02-16 05:28:11 -0600

    model work

---
+++ Pequam.Web/Models/IActivityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pequam.Web.Models
+{
+    interface IActivityRepository
+    {
+        List<Activity> FetchByLoginName(string loginName);
+        void AddActivity(Activity activity);
+    }
+}
